ce5ba66618f00234a497cf6d3b3427e76177526b
HuynhAnhTai <47748453+HuynhAnhTai@users.noreply.github.com>
2020-06-16 21:26:26 +0700

    Fix v1

--- BroadcastInternet/app/src/main/java/com/example/broadcastinternet/BroadcastInternet.kt
+++ BroadcastInternet/app/src/main/java/com/example/broadcastinternet/BroadcastInternet.kt
@@ -1,25 +1,19 @@
 package com.example.broadcastinternet
 
-import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
-import android.widget.Toast
-import androidx.core.content.ContextCompat.getSystemService
-import androidx.core.content.ContextCompat.startActivity
-import androidx.core.os.bundleOf
 
 
+@Suppress("DEPRECATION")
 class BroadcastInternet : BroadcastReceiver() {
 
     override fun onReceive(p0: Context?, p1: Intent?) {
 
-        //if (p1!!.action == "android.net.wifi.WIFI_STATE_CHANGED"){
+        if (p1!!.action.equals("android.net.wifi.WIFI_STATE_CHANGED")) {
             MainActivity.changeState()
-            Toast.makeText(p0, "Hi",Toast.LENGTH_LONG).show()
-        //}
+        }
 
     }
 
